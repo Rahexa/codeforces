b739597feaaf9c7311f451cb58fb795572e92c0f
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 14:59:22 +0600

    Update 1866A - Ambitious Kid.cpp

--- 1866A - Ambitious Kid.cpp
+++ 1866A - Ambitious Kid.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
  using namespace std;
  int main() {
     int n;
-     cin >> n;
+    cin >> n;
      vector<int> arr(n);
-      for (int i = 0; i < n; ++i) {
+     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
      int min_value = abs(arr[0]);
      for (int i = 1; i < n; ++i) {
-        if (abs(arr[i]) < abs (min_value)) {
+        if (abs(arr[i]) < min_value) {
             min_value = abs(arr[i]);
         }
     }
-     cout <<min_value<<endl;
+     cout << min_value << endl;
      return 0;
 }
