c2fa93b6d28d8fa1fc50bccdcbb8a42cf43e0b92
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 14:57:58 +0600

    Add 1894A - Secret Sport.cpp

---
+++ 1894A - Secret Sport.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+int n;
+cin>>n;
+string s;
+cin>>s;
+  if(s[n-1] == 'A')cout<<"A"<<endl;
+else cout<<"B"<<endl;
+}
+}
