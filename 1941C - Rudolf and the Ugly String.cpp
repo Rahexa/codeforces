982b01a28f8c129e82f1e8c0467aa8681e522af4
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 18:31:46 +0600

    Update 1941C - Rudolf and the Ugly String.cpp

--- 1941C - Rudolf and the Ugly String.cpp
+++ 1941C - Rudolf and the Ugly String.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
- int main() {
-    int t;
+#define fast {ios_base::sync_with_stdio(false); cin.tie(0);}
+typedef long long ll;
+ int main()
+{
+    fast
+    ll i, j, k, m, n, c, t, x, y;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
         string s;
-        cin >> s;
-        int count = 0;
-        for (int i = 0; i <= n - 3; i++) {
-            if (s.substr(i, 3) == "map" || s.substr(i, 3) == "pie") {
-                count++;
-            }
-            if (i <= n - 5 && s.substr(i, 5) == "mapie") {
-                count--;
-            }
+        cin >> n >> s;
+        ll ans = 0;
+        for (i = 0; i <= n - 3; i++) {
+            if (s.substr(i, 3) == "map" || s.substr(i, 3) == "pie") ans++;
+            if (i <= n - 5 && s.substr(i, 5) == "mapie") ans--;
         }
-        cout << count << endl;
+        cout << ans << "\n";
     }
     return 0;
 }
