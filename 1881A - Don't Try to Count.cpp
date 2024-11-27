972a7342da07b5717c50a6a2a5ed2d8af39cb138
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 18:51:47 +0600

    Update 1881A - Don't Try to Count.cpp

--- 1881A - Don't Try to Count.cpp
+++ 1881A - Don't Try to Count.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main (){
-int t;
-cin>>t;
-while(t--){
-     int n,m;
-    cin>>n>>m;
-    string x,s;
-    cin>>x>>s;
- string current = x;
-int found = false;
-int pos = 0;
- while(current.size() <= 50){
-    if(current.find(s) !=string::npos){
-        found = true;
-        break;
-     }
-    current += current;
-    pos++;
-  }
- if(found){
-    cout<<pos<<endl;
-   }
- else{
-     cout<<-1<<endl;
-  }
+ int main() {
+    int t;
+    cin >> t; // Number of test cases
+     while (t--) {
+        int n, m;
+        cin >> n >> m; // Lengths of strings x and s
+         string x, s;
+        cin >> x >> s; // Input strings
+         string current = x; // Start with the initial string
+        int operations = 0; // Count of operations
+        bool found = false;
+         // Use while loop to simulate string doubling until s is found
+        while (current.size() <= 50) { // We limit the size of current to avoid excessive memory
+            if (current.find(s) != string::npos) { // If s is found as a substring
+                found = true;
+                break;
+            }
+            current += current; // Double the string
+            operations++;
+        }
+         if (found) {
+            cout << operations << endl; // Output the number of operations
+        } else {
+            cout << -1 << endl; // If s is not found after a reasonable amount of operations
+        }
+    }
+     return 0;
 }
- }
