8e4e3b0cab57dd7dcb5f5a9a1c0933f7b9b53529
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 15:04:44 +0600

    Add 1922A - Tricky Template.cpp

---
+++ 1922A - Tricky Template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+string a,b,c;
+cin>>a>>b>>c;
+ bool ans = false;
+for(int i = 0;i<n ;i++){
+    if(a[i] != c[i] &&b[i] != c[i]) {
+        ans = true;
+                break;
+      }
+  }
+ if(ans) cout<<"YES"<<endl;
+else cout<<"NO"<<endl;
+ }
+}
+                   
