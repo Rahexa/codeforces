eb1fd2f8055267df670486875fb262a6c60e2181
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 15:03:20 +0600

    Add 1877A - Goals of Victory.cpp

---
+++ 1877A - Goals of Victory.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i = 0;i<n-1;i++){
+        cin>>v[i];
+    }
+    int sum = 0;
+    for(int i = 0;i<n-1;i++){
+            sum = sum + v[i];
+    }
+   int result = (0-sum);
+    cout<<result<<endl;
+}
+ }
