15c0944538293f2d2605a9bbd791b607b203e674
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 14:56:22 +0600

    Add 1858A - Buttons.cpp

---
+++ 1858A - Buttons.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(c%2 != 0 && a>b )cout<<"First"<<endl;
+    else if(c%2 != 0 && b>a )cout<<"Second"<<endl;
+       else  if(c%2 != 0 && a==b )cout<<"First"<<endl;
+       else if(c%2 == 0 && a ==b)  cout<<"Second"<<endl;
+     else if(c%2 == 0 && a <b)  cout<<"Second"<<endl;
+          else if(c%2 == 0 && a>b)  cout<<"First"<<endl;
+       }
+    }
