d2a12230fc8ace122656c0e6de521bc9b199c6e2
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 15:03:32 +0600

    Add 1878B - Aleksa and Stack.cpp

---
+++ 1878B - Aleksa and Stack.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+          int t;
+         cin>>t;
+         while(t--){
+            int n;
+            cin>>n;
+          int odd =1;
+      for(int i = 1;i<n+1;i++){
+         cout<<odd<<" "<<endl;
+        odd += 2;
+       }
+             cout<<endl;
+           }
+  }
