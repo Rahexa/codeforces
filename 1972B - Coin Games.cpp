ab834c3b4556c9e7c739a2b32dde2cd8e555a3ad
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 16:59:58 +0600

    Add 1972B - Coin Games.cpp

---
+++ 1972B - Coin Games.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define f first
+#define s second
+#define in insert
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define er erase
+#define pi 3.141592653589793238
+#define MOD 1000000007
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define INF (ll)1000000000000000
+#define all(x) x.begin(),x.end()
+//#define end cout<<endl
+#define bcntz __builtin_ctz
+#define fl(i,n) for(int i=0;i<n;i++)
+using namespace std;
+#define st(s) sort(s.begin(),s.end())
+  void solve()
+{
+   ll n;
+   cin>>n;
+   string s;
+   cin>>s;
+   ll cnt  = 0;
+   for(ll i=0;i<n;i++){
+      if(s[i] == 'U') cnt++;
+   }
+    if(cnt%2) YES;
+   else NO;
+ }
+ int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+   int t;
+  cin>>t;
+  while(t--)
+  {
+     solve();
+  }
+    }
