f8b4c8f246941ca6276baec0dfe36870df0510a9
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 15:23:40 +0600

    Update 1978A - Alice and Books.cpp

--- 1978A - Alice and Books.cpp
+++ 1978A - Alice and Books.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-int t;
-cin>>t;
-while(t--){
-int n;
-cin>>n;
-vector<int>v(n);
-for(int i = 0;i<n;i++){
-    cin>>v[i];
+ int main() {
+    int tt;
+    cin >> tt;  // Read the number of test cases
+    while (tt--) {
+        int n;
+        cin >> n;  // Read the size of the vector
+        vector<int> vec(n);
+         // Read the vector elements
+        for (int i = 0; i < n; i++) {
+            cin >> vec[i];
+        }
+         int ans = vec[n - 1];  // Start with the last element of the vector
+        int mx = -1;
+         // Find the maximum value in the vector from the beginning to the second-to-last element
+        for (int i = 0; i < n - 1; i++) {
+            mx = max(mx, vec[i]);
+        }
+         ans += mx;  // Add the maximum value to ans
+        cout << ans << endl;  // Output the result for the current test case
+    }
+    return 0;
 }
-int max_val = v[0];
-for(int i = 1;i<n-1;i++){
- if(v[i]>max_val){
-    max_val = v[i];
-  }
- }
-  cout<<max_val+v[n-1]<<endl;
-}
-   }
