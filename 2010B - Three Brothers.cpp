8ee2937578c491974d320b5c12927f377f209df0
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-27 14:57:10 +0600

    Add 2010B - Three Brothers.cpp

---
+++ 2010B - Three Brothers.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ int a,b;
+cin>>a>>b;
+ if(a == 3 && b == 1) cout<<2<<endl;
+else if(a==1 && b == 3) cout<<2<<endl;
+else if( a == 2 && b==3)cout<<1<<endl;
+else if(a == 3 && b ==2) cout<<1<<endl;
+else cout<<3<<endl;
+    }
