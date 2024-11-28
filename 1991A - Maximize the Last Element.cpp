eaa3e272a1022daa08987219d6a16e8e2420c01f
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-11-29 01:27:47 +0600

    Update 1991A - Maximize the Last Element.cpp

--- 1991A - Maximize the Last Element.cpp
+++ 1991A - Maximize the Last Element.cpp
@@ -12,7 +12,9 @@ while(t--){
     }
     int max_value = v[0];
      for(int  i = 0;i<n;i = i+2){
-                        max_value = max(max_value, v[i]);
+            if(v[i] > max_value){
+                max_value = v[i];
+            }
     }
      cout<<max_value<<endl;
 }
