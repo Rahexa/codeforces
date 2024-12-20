1c98c5e9fd67f25f3216822f23b427038f171c81
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-12-20 20:02:42 +0600

    Update 2003C - Turtle and Good Pairs.cpp

--- 2003C - Turtle and Good Pairs.cpp
+++ 2003C - Turtle and Good Pairs.cpp
@@ -1,34 +1,34 @@
-    #include <bits/stdc++.h>
-    using namespace std;
-    int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        string s;
-        cin>>s;
-        vector<pair<int,char>>freq(26);
-        for(int i = 0;i<26;i++){
-            freq[i].second = 'a' + i;
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    vector<pair<int,char>>freq(26);
+    for(int i = 0;i<26;i++){
+        freq[i].second = 'a' + i;
+    }
+    for(auto c : s){
+             freq[c -'a'].first++;
+    }
+     sort(freq.rbegin(),freq.rend());
+    string result(n,' ');
+    int indx = 0;
+    for(auto [cnt,ch] : freq){
+        if(cnt == 0){
+            break;
         }
-        for(auto c : s){
-                 freq[c -'a'].first++;
+         for(int i = 0;i<cnt ;i++){
+            if(indx>= n )
+                indx =1;
+            result[indx] = ch;
+            indx +=2;
         }
-         sort(freq.rbegin(),freq.rend());
-        string result(n,' ');
-        int indx = 0;
-        for(auto [cnt,ch] : freq){
-            if(cnt == 0){
-                break;
-            }
-             for(int i = 0;i<cnt ;i++){
-                if(indx>= n ){
-                    indx =1;}
-                result[indx] = ch;
-                indx +=2;
-            }
-         }
-    cout<<result<<endl;
      }
-      }
+cout<<result<<endl;
+ }
+  }
