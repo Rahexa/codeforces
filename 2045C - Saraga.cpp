334af8cda2acece6ad149845e4b8d2a8f4ce0c3f
RAIHAN SIKDER <sikderraihan693@gmail.com>
2024-12-20 20:18:43 +0600

    Update 2045C - Saraga.cpp

--- 2045C - Saraga.cpp
+++ 2045C - Saraga.cpp
@@ -1,50 +1,41 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
- #pragma GCC optimize("Ofast,unroll-loops")
- #define int long long
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
- using namespace std;
-using namespace __gnu_pbds;
- template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
- void solve(){
+using namespace std;
+ void solve() {
     string s, t;
     cin >> s >> t;
-    int n = s.size();
-     // A map to store the index of each character in string `t`
-    map<char, int> m;
+        int n = s.size();
     int x = t.size();
-        // Populate the map with characters from `t` and their corresponding indices
-    for(int i = 0; i < x - 1; i++) m[t[i]] = i;
-     // The variable `ans` will store the smallest abbreviation length found
-    int ans = 1e18; // A large value to compare with
-    int ind = -1;   // The index that gives the smallest abbreviation
-     // Try to find the smallest interesting abbreviation
-    for(int i = 1; i < n; i++){
-        if(m.find(s[i]) != m.end()){ // Check if the character `s[i]` exists in `t`
-            // Calculate the abbreviation length
-            if(ans > x - m[s[i]] + i){
-                ans = x - m[s[i]] + i;
+        map<char, int> m; // Map to store the index of each character in t
+    for(int i = 0; i < x - 1; i++) {
+        m[t[i]] = i;
+    }
+     int ans = 1e18;
+    int ind = -1;
+     // Find the smallest interesting abbreviation
+    for(int i = 1; i < n; i++) {
+        if(m.find(s[i]) != m.end()) {
+            int len = x - m[s[i]] + i;
+            if(ans > len) {
+                ans = len;
                 ind = i;
             }
         }
-    }   
-     if(ind == -1){ // If no valid abbreviation was found
+    }
+     // Output result
+    if(ind == -1) {
         cout << -1 << endl;
         return;
     }
-        // Construct the final abbreviation
-    string f = "";
-        // First part: Prefix of `s` up to index `ind`
+     string f = "";
+    // Prefix part from s[0..ind]
     for(int i = 0; i <= ind; i++) f.push_back(s[i]);
-     // Second part: Suffix of `t` starting after the index `m[s[ind]]`
+    // Suffix part from t starting after index m[s[ind]]
     for(int i = m[s[ind]] + 1; i < x; i++) f.push_back(t[i]);
-     cout << f << endl; // Output the abbreviation
+     cout << f << endl;
 }
- signed main(){
+ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-        solve(); // Call the solve function to run the problem-solving logic
-        return 0;
+    solve();
+    return 0;
 }
